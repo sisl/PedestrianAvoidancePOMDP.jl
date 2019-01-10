b9186f31a5506536a903b7bd3197ecdd0e74ca21
Schratter Markus <markus.schratter@v2c2.at>
2019-01-10 05:47:33 +0100

    added collisioncost ot evaluation scripts

--- src/lateral_script.sh
+++ src/lateral_script.sh
@@ -2,13 +2,14 @@
 
 # investigation collision speed
 
+collisioncost=-600.0
+
 policy_name="lateral3"
+
 #for algorithm in "PedestrianAvoidancePOMDP"
-#for algorithm in "PedestrianAvoidancePOMDP_EmergencyBrakingSystem"
-for algorithm in "EmergencyBrakingSystem"
+for algorithm in "PedestrianAvoidancePOMDP_EmergencyBrakingSystem"
 do
     echo "algorithm: " $algorithm
-
 
     keepvelocityreward=50.0
     actionloncost=-2.0
@@ -18,7 +19,7 @@ do
         do
             for  keeplanereward in 10.0 20.0 50.0 100.0 
             do
-                julia1.0 simulate_scenarios.jl --algorithm=$algorithm --policy_name=$policy_name --pedestrianbirthprobability=$p_pedestrian --actionloncost=$actionloncost --actionlatcost=$actionlatcost --keepvelocityreward=$keepvelocityreward --keeplanereward=$keeplanereward
+                julia1.0 simulate_scenarios.jl --algorithm=$algorithm --policy_name=$policy_name --collisioncost=$collisioncost --pedestrianbirthprobability=$p_pedestrian --actionloncost=$actionloncost --actionlatcost=$actionlatcost --keepvelocityreward=$keepvelocityreward --keeplanereward=$keeplanereward
             done
         done
     done
--- src/longitudinal_script.sh
+++ src/longitudinal_script.sh
@@ -2,22 +2,24 @@
 
 # investigation collision speed
 
+collisioncost=-600.0
+
 policy_name="longitudinal"
 
-for algorithm in "PedestrianAvoidancePOMDP"
-#for algorithm in "PedestrianAvoidancePOMDP_EmergencyBrakingSystem"
+#for algorithm in "PedestrianAvoidancePOMDP"
+for algorithm in "PedestrianAvoidancePOMDP_EmergencyBrakingSystem"
 do
     echo "algorithm: " $algorithm
 
     keeplanereward=0.0
     actionlatcost=0.0
-    for keepvelocityreward in 0.0 20.0 50.
+    for keepvelocityreward in 0.0 10.0 20.0 30.0 40.0
     do
-        for actionloncost in 0.0 -2.0 -5.0 -10.
+        for actionloncost in 0.0 -1.0 -2.0 -5.0
         do
             for p_pedestrian in 0.1 0.3 0.5 0.7 0.9
             do
-                julia1.0 simulate_scenarios.jl --algorithm=$algorithm --policy_name=$policy_name --pedestrianbirthprobability=$p_pedestrian --actionloncost=$actionloncost --actionlatcost=$actionlatcost --keepvelocityreward=$keepvelocityreward --keeplanereward=$keeplanereward
+                julia1.0 simulate_scenarios.jl --algorithm=$algorithm --policy_name=$policy_name --collisioncost=$collisioncost --pedestrianbirthprobability=$p_pedestrian --actionloncost=$actionloncost --actionlatcost=$actionlatcost --keepvelocityreward=$keepvelocityreward --keeplanereward=$keeplanereward
             done
         done
     done
